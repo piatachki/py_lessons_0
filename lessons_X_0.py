c618541841dc09f87852c9621eb312e47a27a55b
Vasily Batishchev <piatachki@gmail.com>
2021-06-08 20:01:38 +0300

    Разарботка методов пользовательского ввода

--- lessons_X_0.py
+++ lessons_X_0.py
@@ -1,12 +1,16 @@
 # Константы / настройки
-x_dimension = 3
-y_dimension = 3
+# Размерность карты
+dimension = 3
+# Значение пустой клетки
+placeholder = ' '
 
 
 # Инициализация объекта поля для новой игры
 def init_field():
-    row = [' '] * x_dimension
-    field = [row.copy(), row.copy(), row.copy()]
+    field = []
+    for i in range(dimension):
+        row = placeholder * dimension
+        field.append(row)
     return field
 
 
@@ -15,18 +19,34 @@ def print_field(field):
     print("")
     print("   1 2 3")
     for i in field:
-        print(f" - { i[0] } { i[1] } { i[2] } ")
+        print(f" - {i[0]} {i[1]} {i[2]} ")
     print("")
 
 
-# Функция получения координат от пользователя, валидации и записи в поле
-def input_coords(field, play_side):
-    user_input = list(map(int, str.split(input(f"Ход {play_side}: введите координаты x y"))))
-    y, x = user_input[0], user_input[1]
-    if x <= len(field) and y <= len(field[0]) and field[x-1][y-1] == ' ':
-        field[x-1][y-1] = play_side
+# Метод получения координат от пользователя, валидации и записи в поле
+def input_coords(play_side):
+    user_input = ''
+    while not validate_user_input(user_input):
+        user_input = input(f"Ход {play_side}: введите координаты x y")
+    return parse_user_input(user_input)
 
 
-field = init_field()
-input_coords(field, 'X')
-print_field(field)
+# Метод валидации строки пользовательского ввода
+def validate_user_input(user_input):
+    user_input_list = str.split(user_input)
+    return len(user_input_list) == 2 and \
+           str.isdigit(user_input_list[0]) and \
+           str.isdigit(user_input_list[1]) and \
+           0 < int(user_input_list[0]) <= dimension and \
+           0 < int(user_input_list[1]) <= dimension
+
+
+# Метод парсинга пользовательского воода
+def parse_user_input(user_input):
+    user_input_list = str.split(user_input)
+    return int(user_input_list[0]), int(user_input_list[1])
+
+
+global_field = init_field()
+print(input_coords('X'))
+# print_field(global_field)
