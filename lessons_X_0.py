ba10c61fb61d56c209cdc94612f9085ab8840dcc
Vasily Batishchev <piatachki@gmail.com>
2021-06-08 20:44:43 +0300

    Разарботка методов пользовательского ввода

--- lessons_X_0.py
+++ lessons_X_0.py
@@ -1,5 +1,7 @@
 # Константы / настройки
 # Размерность карты
+import itertools
+
 dimension = 3
 # Значение пустой клетки
 placeholder = ' '
@@ -9,17 +11,18 @@ placeholder = ' '
 def init_field():
     field = []
     for i in range(dimension):
-        row = placeholder * dimension
+        row = [placeholder] * dimension
         field.append(row)
     return field
 
 
 # Вывод карты на экран
+# ToDo сделать независимым от размера поля
 def print_field(field):
     print("")
     print("   1 2 3")
     for i in field:
-        print(f" - {i[0]} {i[1]} {i[2]} ")
+        print(f" idx {i[0]} {i[1]} {i[2]} ")
     print("")
 
 
@@ -27,7 +30,7 @@ def print_field(field):
 def input_coords(play_side):
     user_input = ''
     while not validate_user_input(user_input):
-        user_input = input(f"Ход {play_side}: введите координаты x y")
+        user_input = input(f"Ход {play_side}: введите координаты: x y\n")
     return parse_user_input(user_input)
 
 
@@ -41,12 +44,39 @@ def validate_user_input(user_input):
            0 < int(user_input_list[1]) <= dimension
 
 
+# Метод проверки, что планируемая к заполнению ячейка не занята
+def validate_field_place(examine_coords):
+    return global_field[examine_coords[0]][examine_coords[1]] == placeholder
+
+
 # Метод парсинга пользовательского воода
 def parse_user_input(user_input):
     user_input_list = str.split(user_input)
-    return int(user_input_list[0]), int(user_input_list[1])
+    return int(user_input_list[0]) - 1, int(user_input_list[1]) - 1
 
 
+# Метод проверки того, что кортеж полон и
+# полностью заполнен символом одного из игроков
+def check_tuple_win(*args):
+    distinct_args = set(args)
+    return len(args) == dimension and \
+           len(distinct_args) == 1 and \
+           placeholder not in distinct_args
+
+
+# Генератор для определения очередности ходов
+def get_play_side():
+    side_sequence = 'X0'
+    for play_side in itertools.cycle(side_sequence):
+        yield play_side
+
+
+# Точка входа
 global_field = init_field()
-print(input_coords('X'))
-# print_field(global_field)
+for side in get_play_side():
+    coords = input_coords(side)
+    while not validate_field_place(coords):
+        print(f"Координаты { coords[0] } { coords[1] } уже заняты. Нужно бы повторить ход")
+        coords = input_coords(side)
+    global_field[coords[0]][coords[1]] = side
+    print_field(global_field)
